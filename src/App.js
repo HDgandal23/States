f0ce2826fd3d9977e5debc85fb513d6c587cb9dd
Habib <habibdiallohd08@gmail.com>
2024-02-14 19:45:13 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      
+      personne: {
+        fullName: "Pro",
+        bio: "Gandal-Easy builder",
+        imgSrc: "https://c.files.bbci.co.uk/17A2C/production/_105621869_sonkoafpgetty.jpg",
+        profession: "Développeur",
+      },
+      show: false,
+      temps: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        temps: prevState.temps + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  basculer = () => {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.personne;
+    const { show, temps } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.basculer}> Basculer </button>
+        {show && (
+          <div>
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <img src={imgSrc} alt={fullName} />
+            <p>{profession}</p>
+          </div>
+        )}
+        <p>Temps : {temps} s</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
